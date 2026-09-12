323d6f0f9d80b68261f7349d4dffd283c2634ae7
Ambika Krishna Ullamgunta <ambikakrishna2004@gmail.com>
2026-09-12 21:02:41 +0530

    Updated a simple select query to get the details of an particular employee ID

--- Employee_Management_System.sql
+++ Employee_Management_System.sql
@@ -45,6 +45,7 @@ select * from employee order by salary asc;
 select * from employee order by salary desc;
 
 
+select * from employee where emp_id = 103;
 select * from employee where city = 'Hyderabad';
 select * from employee where city = 'Bangalore';
 
