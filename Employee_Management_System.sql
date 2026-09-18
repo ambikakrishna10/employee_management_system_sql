ead6b4922b7413ec50646fe3fd22b669f525cb7c
Ambika Krishna Ullamgunta <ambikakrishna2004@gmail.com>
2026-09-18 23:00:12 +0530

    Updated a simple select query using where

--- Employee_Management_System.sql
+++ Employee_Management_System.sql
@@ -48,6 +48,7 @@ select * from employee order by salary desc;
 select * from employee where emp_id = 103;
 select * from employee where dept_id = 1;
 select * from employee where dept_id = 2;
+SELECT * FROM employee WHERE dept_id IN (1, 3);
 select * from employee where city = 'Hyderabad';
 select * from employee where city = 'Hyderabad' and salary > 50000;
 select * from employee where city = 'Bangalore';
